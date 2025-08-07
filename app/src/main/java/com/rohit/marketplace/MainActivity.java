5cc91093c029837d70d857a639b489dc2097803e
Rohit <rohitrawat590590@gmail.com>
2025-08-07 23:44:53 +0530

    first commit

---
+++ app/src/main/java/com/rohit/marketplace/MainActivity.java
@@ -0,0 +1,18 @@
+package com.rohit.marketplace;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://ecommerce-app-7.vercel.app");
+        setContentView(webView);
+    }
+}
